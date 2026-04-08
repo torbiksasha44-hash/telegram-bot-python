d08d032d1b7df12dd71a33fb585568dd81538954
torbiksasha44-hash <torbiksasha44@gmail.com>
2026-04-08 12:12:58 +0200

    Remove token print statement
    Removed print statement for the token.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import telebot
 
 TOKEN = os.getenv("8709005198:AAG0SB65EJZR6leqzNpCL3RYpi8_7_ipeKo")
 
-print("TOKEN:", TOKEN)
-
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
