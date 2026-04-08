e5a624528e6c988a8cffb617cca4a67aa5920211
torbiksasha44-hash <torbiksasha44@gmail.com>
2026-04-08 12:40:51 +0200

    Implement basic Telegram bot features
    Added Telegram bot functionality with start command and message echo.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-bot.send_message(message.chat.id, "NEW CODE TEST 🚀")
+import os
+import telebot
+
+# Получаем токен из Railway Variables
+TOKEN = os.getenv("8709005198:AAG0SB65EJZR6leqzNpCL3RYpi8_7_ipeKo")
+
+# Создаём бота
+bot = telebot.TeleBot(TOKEN)
+
+# Команда /start
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот работает ✅")
+
+# Ответ на любые сообщения
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, message.text)
+
+# Запуск
+bot.polling()
