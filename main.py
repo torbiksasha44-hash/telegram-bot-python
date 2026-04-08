6120a38d3e460ee35206152a50f40bc56f19d1d9
torbiksasha44-hash <torbiksasha44@gmail.com>
2026-04-08 12:24:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-import os
-import telebot
-
-# Берём токен из Railway Variables
-TOKEN = os.getenv("8709005198:AAG0SB65EJZR6leqzNpCL3RYpi8_7_ipeKo")
-
-# Создаём бота
-bot = telebot.TeleBot(TOKEN)
-
-# Команда /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Бот работает ✅")
-
-# Ответ на любые сообщения
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
-
-# Запуск бота
-bot.polling()
+    bot.send_message(message.chat.id, "НОВЫЙ КОД РАБОТАЕТ 🚀")
