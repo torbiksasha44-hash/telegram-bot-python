3796f4a966fc6a7b1244bfc9641dca6504d0cf02
torbiksasha44-hash <torbiksasha44@gmail.com>
2026-04-08 12:10:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import os
 import telebot
 
-TOKEN = os.getenv("8709005198:AAEIw9YwBKi1iRYn2oQlw_8CR_OWe9XD3ms")
+TOKEN = os.getenv("8709005198:AAG0SB65EJZR6leqzNpCL3RYpi8_7_ipeKo")
+
+print("TOKEN:", TOKEN)
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Бот работает ✅")
-
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.send_message(message.chat.id, message.text)
 
 bot.polling()
